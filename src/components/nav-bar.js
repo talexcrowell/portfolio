eccb7c294bd1b79b588ef693b9c3624cdd9ca08b
talexcrowell <talexcrowell@gmail.com>
2019-02-14 11:35:01 -0800

    fixed mobile design

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -6,12 +6,11 @@ export default class NavBar extends React.Component{
       <section className='nav-bar'>
         <ul className='nav-directionals'>
           <li className='nav-nameplate'>T. Alex Crowell</li>
-          <li className='nav-direction'><a href='https://docs.google.com/document/d/1h5_sHImeKSV5LLLN5Pat8KDdU8bqAvYXHSJL7hCCiuU/edit?usp=sharing' target='_blank' rel="noopener noreferrer"><i className='nav-resume'/></a></li>
-          <li className='nav-direction'><a href='mailto:talexcrowell@gmail.com'><i className='nav-email'/></a></li>
-          <li className='nav-direction-right'><a href='#contact'>Contact</a></li>
-          <li className='nav-direction-right'><a href='#about'>About Me</a></li> 
           <li className='nav-direction-right'><a href='#portfolio'>Portfolio</a></li>
-        </ul> 
+          <li className='nav-direction-right'><a href='#about'>About Me</a></li>
+          <li className='nav-direction-right'><a href='#contact'>Contact</a></li>
+          <li className='nav-direction-right hide'><a href='https://docs.google.com/document/d/1h5_sHImeKSV5LLLN5Pat8KDdU8bqAvYXHSJL7hCCiuU/edit?usp=sharing' target='_blank' rel="noopener noreferrer"><p className='nav-resume-text'>Resume</p><i className='nav-resume'/></a></li>
+        </ul>
       </section>
     )
   }
