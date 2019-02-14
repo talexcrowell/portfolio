cc50b94ec13b3471a2780e224f8186a32b101b40
talexcrowell <talexcrowell@gmail.com>
2019-02-14 08:24:26 -0800

    fixed icon hovers

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -13,9 +13,12 @@ export default class AboutMe extends React.Component{
         <p className='about-description'>
         I was born and raised in Albuquerque, New Mexico and attended University of New Mexico where I graduated with my Bachelor's of Science in Biology. 
         After a few years of different jobs, I felt the need to discover more about myself and how I can use technology to help others.
-        The key to achieving those goals is to work with others to  learn and build a quality project. Working with a team brings an opportunity to learn new techniques, understand different approaches to a problem, and potentially teach others some personal tricks to add to their engineering toolbox.
-        This led me to enter the the world of web development and cyber security.
         I am passionate about innovation and protecting people in their everyday use of digital resources.
+        </p>
+        <p className='about-description'>
+        This led me to enter the the world of web development and cyber security.
+        The key to achieving these goals is to work with others and build a quality project. 
+        Working with a team brings an opportunity to learn new techniques, understand different approaches to a problem, and potentially teach others some personal tricks to add to their engineering toolbox.
         </p>
         <p className='about-description'>
         Thank you for visiting my portfolio and please feel free to contact me if you have any questions :)  
