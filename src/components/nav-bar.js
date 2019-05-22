358110c3c1bf3682e45ce3b0a762643379cf449d
talexcrowell <talexcrowell@gmail.com>
2019-05-22 10:56:40 -0700

    updated resume link

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -9,7 +9,7 @@ export default class NavBar extends React.Component{
           <li className='nav-direction-right'><a href='#portfolio'>Portfolio</a></li>
           <li className='nav-direction-right'><a href='#about'>About Me</a></li>
           <li className='nav-direction-right'><a href='#contact'>Contact</a></li>
-          <li className='nav-direction-right hide'><a href='https://docs.google.com/document/d/1h5_sHImeKSV5LLLN5Pat8KDdU8bqAvYXHSJL7hCCiuU/edit?usp=sharing' target='_blank' rel="noopener noreferrer"><p className='nav-resume-text'>Resume</p><i className='nav-resume'/></a></li>
+          <li className='nav-direction-right hide'><a href='https://docs.google.com/document/d/1tvsq0Nxspdy9KWSLqVnAV7gKd_KKHxzIxxNLwo4rI-g/edit?usp=sharing' target='_blank' rel="noopener noreferrer"><p className='nav-resume-text'>Resume</p><i className='nav-resume'/></a></li>
         </ul>
       </section>
     )
